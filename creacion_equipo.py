3c087d29ca6ebdc7ae180fb5272af86f2be7c780
sergio <sergio.chulianmantel@iesdonana.org>
2021-03-19 16:58:19 +0100

    Modulo main y creacion_equipo separados y funcionando

---
+++ creacion_equipo.py
@@ -0,0 +1,68 @@
+from entrenador import Entrenador
+from pokemon import Pokemon
+from pokedex import *
+from movimientos import *
+import random
+from combate import Combate
+
+disponibles = ['vaporeon', 'flareon', 'jolteon', 'eevee']
+disponibles_jugador = disponibles[::]
+disponibles_rival = disponibles[::]
+
+def mostrar_disponibles():
+    """Imprime en la salida la lista de pokemon disponibles"""
+    for i in disponibles_jugador:
+        print(i)
+
+def quitar_disponible(nombre, lista):
+    """Quita de la lista de disponibles el pokemon seleccionado"""
+    lista.remove(nombre)
+
+def despachar_pokemon(pk, lista):
+    """Devuelve el pokemon seleccionado"""
+    if pk not in lista:
+        raise ValueError('El pokemon elegido no se encuentra disponible')
+    elif pk == 'vaporeon':
+        quitar_disponible('vaporeon', lista)
+        return Vaporeon()
+    elif pk == 'flareon':
+        quitar_disponible('flareon', lista)
+        return Flareon()
+    elif pk == 'jolteon':
+        quitar_disponible('jolteon', lista)
+        return Jolteon()
+    elif pk == 'eevee':
+        quitar_disponible('eevee', lista)
+        return Eevee()
+
+
+def seleccionar_pokemon_jugador():
+    """Devuelve una lista con 3 instancias de la clase Pokemon que será el equipo del jugador"""
+    pk1 = None
+    pk2 = None
+    pk3 = None
+
+    seleccion = [pk1, pk2, pk3]
+    contador = 0
+
+    while contador < 3:
+        try:
+            seleccion[contador] = despachar_pokemon(input('Elige un pokemon: '), disponibles_jugador)
+        except ValueError:
+            print('\nEl pokemon elegido no se encuentra disponible. Elige uno de los siguientes:')
+            mostrar_disponibles()
+        else:
+            contador += 1
+        finally:
+            print()
+            continue
+    return seleccion
+
+def seleccionar_pokemon_rival():
+    """Devuelve una lista con 3 instancias de la clase pokemon que será el equipo del rival"""
+    seleccion = []
+    for i in range(0, 3):
+        aleatorio = random.choice(disponibles_rival)
+        poke = despachar_pokemon(aleatorio, disponibles_rival)
+        seleccion.append(poke)
+    return seleccion
--- main.py
+++ main.py
@@ -1,84 +1,20 @@
-from entrenador import Entrenador
-from pokemon import Pokemon
-from pokedex import *
-from movimientos import *
+import creacion_equipo
 import random
-from combate import Combate
-
-disponibles = ['vaporeon', 'flareon', 'jolteon', 'eevee']
-disponibles_jugador = disponibles[::]
-disponibles_rival = disponibles[::]
-
-def mostrar_disponibles():
-    """Imprime en la salida la lista de pokemon disponibles"""
-    for i in disponibles_jugador:
-        print(i)
-
-def quitar_disponible(nombre, lista):
-    """Quita de la lista de disponibles el pokemon seleccionado"""
-    lista.remove(nombre)
-
-def despachar_pokemon(pk, lista):
-    """Devuelve el pokemon seleccionado"""
-    if pk not in lista:
-        raise ValueError('El pokemon elegido no se encuentra disponible')
-    elif pk == 'vaporeon':
-        quitar_disponible('vaporeon', lista)
-        return Vaporeon()
-    elif pk == 'flareon':
-        quitar_disponible('flareon', lista)
-        return Flareon()
-    elif pk == 'jolteon':
-        quitar_disponible('jolteon', lista)
-        return Jolteon()
-    elif pk == 'eevee':
-        quitar_disponible('eevee', lista)
-        return Eevee()
-
-
-def seleccionar_pokemon(lista):
-    """Devuelve una lista con 3 instancias de la clase Pokemon"""
-    pk1 = None
-    pk2 = None
-    pk3 = None
-
-    seleccion = [pk1, pk2, pk3]
-    contador = 0
-
-    while contador < 3:
-        try:
-            seleccion[contador] = despachar_pokemon(input('Elige un pokemon: '), lista)
-        except ValueError:
-            print('\nEl pokemon elegido no se encuentra disponible. Elige uno de los siguientes:')
-            mostrar_disponibles(lista)
-        else:
-            contador += 1
-        finally:
-            print()
-            continue
-    return seleccion
-
-def seleccionar_pokemon_rival(lista):
-    seleccion = []
-    for i in range(0, 3):
-        aleatorio = random.choice(lista)
-        poke = despachar_pokemon(aleatorio, lista)
-        seleccion.append(poke)
-    return seleccion
-
 
 print('Hola entrenador, preparate para combatir!\n')
 print('Pokemon disponibles:')
-mostrar_disponibles()
+creacion_equipo.mostrar_disponibles()
 print()
 
-equipo = seleccionar_pokemon(disponibles_jugador)
-equipo_rival = seleccionar_pokemon_rival(disponibles_rival)
+#Pedimos al jugador que seleccione su equipo:
+__equipo = creacion_equipo.seleccionar_pokemon_jugador()
+__equipo_rival = creacion_equipo.seleccionar_pokemon_rival()
+__jugador = creacion_equipo.Entrenador(__equipo[0], __equipo[1], __equipo[2])
+__rival = creacion_equipo.Entrenador(__equipo_rival[0], __equipo_rival[1], __equipo_rival[2])
 
-jugador = Entrenador(equipo[0], equipo[1], equipo[2])
-rival = Entrenador(equipo_rival[0], equipo_rival[1], equipo_rival[2])
+#Se crea el combate:
+combate = creacion_equipo.Combate(__jugador, __rival)
 
-combate = Combate(jugador, rival)
-
+#Se inicia el combate:
 while not combate.get_terminado():
     combate.siguiente_turno()
